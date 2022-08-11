6be97180138ca141983ed55852acc46a95f86aee
edinelson luis de sousa junior <edinelsonjr.ufopa@gmail.com>
2022-08-11 10:01:46 -0300

    Organizando o projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+export default function Home() {
+  /**
+   * Função inicial do nosso aplicativo
+   * Interface é componente: elemento visual
+   *
+   * Densidade de Pixels:
+   *
+   *  */
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>React Native</Text>
+      <Text style={styles.eventDate}>11/08/1994</Text>
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 30,
+    },
+    titulo: {
+      fontSize: 23,
+      color: "#fff",
+    },
+    eventDate: {
+      fontStyle: "italic",
+      fontSize: 15,
+      color: '#f1f2f3'
+    }
+  });
+  
